4180460dfc6123e2ca8020e28fe599e852db6b50
o-o <22546578+Apollo-o@users.noreply.github.com>
2019-05-28 16:06:55 -0700

    Update fuzz.py

--- Tools/fuzz.py
+++ Tools/fuzz.py
@@ -9,25 +9,41 @@ import re
 # Precondition: A String.
 # Postcondition: A List.
 def link_crawler(url):
-    html  = requests.get(url)
-    links = re.findall(r'href=[\'"]?([^\'" >]+)',str(html.content))
-    print(len(links))
-    links = link_formatter(url,links)
-    return links
+    try:
+        html  = requests.get(url)
+        links = re.findall(r'href=[\'"]?([^\'" >]+)',str(html.content))
+        links = link_formatter(url,links)
+        print(len(links))
+        return links
+    except Exception as e:
+        print(e)
 
+# Formats Links.
+# Precondition: A String | A List.
+# Postcondition: A List.
 def link_formatter(url,links):
-  temp = []
-  for link in links:
-    if "https" in link:
-      temp.append(link)
-    elif "http" in link:
-      temp.append(link[:4] + "s" + link[4:])
-    elif link[0] == "/":
-      temp.append(url + link[1:])
-    elif link[0] == "#":
-      temp.append(url + link)
-  return temp
+    temp = []
+    for link in links:
+        if "https" in link:
+            temp.append(link)
+        elif "http" in link:
+            temp.append(link[:4] + "s" + link[4:])
+        elif link[0] == "/" or link[0] == "#":
+            temp.append(url + link)
+        else:
+            print(link)
+    print(len(links))
+    return temp
 
-url   = "https://www.tacoma.uw.edu/"
-links = link_crawler(url)
-print(len(links))
+# Start Program.
+# Precondition: A String.
+# Postcondition: Files.
+def main():
+    # Homepage Links.
+    url   = "https://www.tacoma.uw.edu"
+    links = link_crawler(url)
+
+    # Crawl Entire Site.
+    for link in links:
+        links = link_crawler(link)
+main()
