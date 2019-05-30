9c4e61a0d11a45de09d149ef361d7a7746cc841a
o-o <22546578+Apollo-o@users.noreply.github.com>
2019-05-30 05:37:50 -0700

    Update fuzz.py

--- Tools/fuzz.py
+++ Tools/fuzz.py
@@ -21,36 +21,56 @@ def link_crawler(url):
 # Postcondition: A List.
 def link_formatter(url,links):
     temp = []
-    for link in links:
-        if "https" in link:
-            temp.append(link.strip())
-        elif "http" in link:
-            temp.append(link[:4] + "s" + link[4:].strip())
-        elif link[0] == "/" or link[0] == "#":
-            temp.append(url + link)
-        else:
-            print("",end="")
-
-    return temp
+    if links != None:
+        for link in links:
+            #print(link)
+            if "https" in link:
+                temp.append(link.strip())
+            elif "http" in link:
+                temp.append(link[:4] + "s" + link[4:].strip())
+            elif link[0] == "/" or link[0] == "#":
+                temp.append(url + link)
+            else:
+                print("",end="")
+        return temp
 
 def email_crawler(links):
-    temp = [link[7:].strip() for link in links if "mailto:" in link]
-    return temp
+    temp = []
+    if links != None:
+        temp = [link[7:].strip() for link in links if "mailto:" in link and not("#" in link)]
+        return temp
 
 # Start Program.
 # Precondition: A String.
 # Postcondition: Files.
 def main():
+
+    # Storage.
+    email = []
+    links = []
     # Homepage Links.
-    url = "https://www.tacoma.uw.edu"
+    url = "https://github.com"
     rlinks = link_crawler(url)
     emails = email_crawler(rlinks)
     nlinks = link_formatter(url,rlinks)
 
     # Crawl Entire Site.
+    count = 0
     for link in nlinks:
         rlinks = link_crawler(link)
         emails = email_crawler(rlinks)
         nlinks = link_formatter(url,rlinks)
-        print(len(rlinks),len(nlinks),len(emails))
+        
+        count +=1
+        
+        if rlinks != None:
+            if len(emails) >=1 and len(nlinks) >=1:
+                email.append(emails)
+                links.append(nlinks)
+        elif count == 20:
+            break
+    email.sort()
+    print(email)
+    #links.sort()
+    #print(links)
 main()
