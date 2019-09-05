f2458ccda28b194ed6243cf430189cd33ee509e0
o-o <22546578+Apollo-o@users.noreply.github.com>
2019-09-04 17:59:52 -0700

    Update spider.py

--- Tools/Recon/Websites/Engines/spider.py
+++ Tools/Recon/Websites/Engines/spider.py
@@ -20,9 +20,7 @@ def dork():
 
     # Assign User Agent.
     user_agent = {'User-Agent':
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3)\
-                  AppleWebKit/537.36 (KHTML, like Gecko)\
-                  Chrome/35.0.1916.47 Safari/537.36'}
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
 
     # Assign Proxies.
     response   = requests.get('https://free-proxy-list.net/', headers=user_agent)
