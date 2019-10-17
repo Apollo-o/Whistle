02f31f642538617dab4d4b6a00d72e2a62d66718
o-o <22546578+Apollo-o@users.noreply.github.com>
2019-10-17 12:33:23 -0700

    Create test.py

---
+++ Tools/test.py
@@ -0,0 +1,6 @@
+# ANSI Escape Characters: http://ozzmaker.com/add-colour-to-text-in-python/
+
+url = "https://repl.it/languages/Python3"
+print("\033[0;32;40m {}".format(url))
+print("\033[0;33;40m {}".format(url))
+print("\033[0;31;40m {}".format(url))
